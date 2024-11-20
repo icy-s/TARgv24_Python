7e1fbca78a575d15e2db9244fcd474e212afc46b
icy-s <evgeny.tailov@gmail.com>
2024-11-20 16:36:58 +0200

    ülesanned

--- TARgv24_Python/TARgv24_Python.py
+++ TARgv24_Python/TARgv24_Python.py
@@ -1,6 +1,9 @@
+from random import * # * - kõik funktsioonid, as - funktsioonide ümbernimetus
+from math import * #pi kasutamiseks
+
 #ülesanne 1
 # print("Tere tulemast!")
-# nimi = input("Mis on sinu nimi? ")
+# nimi = input("Mis on sinu nimi? ").capitalize() #lower(),upper(),
 # print("Tere tulemast! Tervitan sind ", nimi)
 # print("Tere tulemast! Tervitan sind "+ nimi)
 # vanus=int(input("Kui vana sa oled? "))
@@ -18,3 +21,15 @@
 # print(type(kas_käib_koolis))
 
 #ülesanne 3
+# kokku=randint(1,1000)
+# print(f"kokku on {kokku} kommi")
+# kommi=int(input("mitu kommi sa tahad? "))
+# kokku=kokku-kommi
+# print(f"jääk on {kokku} kommi")
+
+#ülesanne 4
+# print("Läbimõõdu leidmine ")
+# #l - ümbermõõt
+# l=float(input("Ümbermõõt: "))
+# d=l/pi
+# print(f"Läbimõõdu suurus on {round(d,2)}")
