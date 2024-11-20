63d24d60d0419f1fa22cc1a2928d91015a44a89a
icy-s <evgeny.tailov@gmail.com>
2024-11-20 15:51:30 +0200

    Add project files.

---
+++ TARgv24_Python/TARgv24_Python.py
@@ -0,0 +1,8 @@
+#ülesanne 1
+print("Tere tulemast!")
+nimi = input("Mis on sinu nimi? ")
+print("Tere tulemast! Tervitan sind ", nimi)
+print("Tere tulemast! Tervitan sind "+ nimi)
+vanus=int(input("Kui vana sa oled? "))
+print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastat vana")
+print(f"Tere tulemast! Tervitan sind {nimi} Sa oled {vanus} aastat vana")
