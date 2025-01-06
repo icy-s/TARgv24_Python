9223042b05c74310606feb9f9cdb8aa338b1171d
icy-s <evgeny.tailov@gmail.com>
2025-01-06 16:10:39 +0200

    repka

---
+++ TARgv24_Python/repka.py
@@ -0,0 +1,10 @@
+vanaisa = "Vanaisa"
+vanaema = "Vanaema"
+laps = "Laps"
+koer = "Koer"
+kass = "Kass"
+hiir = "Hiir"
+tegelased = [vanaisa, vanaema, laps, koer, kass, hiir]
+for tegelane in tegelased:
+    print(f"{tegelane} aitab repkat tõmmata.")
+print(f"Kõik koos tõmbasid repka välja!")
