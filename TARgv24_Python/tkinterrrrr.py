0681c2bcc6236c07a073b8c6a2e754ab8cca4169
icy-s <evgeny.tailov@gmail.com>
2025-02-25 22:18:43 +0200

    in progress

---
+++ TARgv24_Python/tkinterrrrr.py
@@ -0,0 +1,17 @@
+from tkinter import *
+kvadrat = Tk()
+kvadrat.geometry("1000x500")
+tablichka = Label(kvadrat,text="решай",font=("Bernadette",50),bg='green')
+tablichka.place(x=250, width=500, height=100)
+vvod1 = Entry(kvadrat,font=50,bg='green')
+vvod1.place(x=50,y=180, width=100,height=100)
+uravnenie = Label(kvadrat,text="x**2+",font=("Bernadette",50),bg='green')
+uravnenie.place(x=200,y=195)
+vvod2 = Entry()
+
+
+
+
+
+
+kvadrat.mainloop()
